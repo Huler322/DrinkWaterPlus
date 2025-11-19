2b79650dae55873797f60c970d45aaec5dc720b4
Anatoliy Tarnovskiy <tarnovskidj@gmail.com>
2025-11-19 12:45:32 +0100

    reinit project

---
+++ Aquaminder Watch App/AquaminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaminderApp.swift
+//  Aquaminder Watch App
+//
+//  Created by Anatoliy Tarnovskiy on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Aquaminder_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
